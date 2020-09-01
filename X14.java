0efc021de9c39e51dded9b6c1858732fd899993c
laPronoia <70538655+laPronoia@users.noreply.github.com>
2020-09-01 11:05:18 +0200

    14. American flag on the screen
    14. Write a Java program to print an American flag on the screen. Go to the editor
    Expected Output
    
    * * * * * * ==================================                          
     * * * * *  ==================================                          
    * * * * * * ==================================                          
     * * * * *  ==================================                          
    * * * * * * ==================================                          
     * * * * *  ==================================                          
    * * * * * * ==================================                          
     * * * * *  ==================================                          
    * * * * * * ==================================                          
    ==============================================                          
    ==============================================                          
    ==============================================                          
    ==============================================                          
    ==============================================                          
    ==============================================

---
+++ X14.java
@@ -0,0 +1,36 @@
+import java.lang.Math.*;
+import java.util.Scanner;
+
+public class X14 {
+ public static void main(String[] args) {
+
+
+  USAflag();
+
+
+ } // End main
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------------------------------------
+
+  static void USAflag() {
+
+   for (int i = 0; i <= 2; i++) {
+     System.out.println(" *  *  *  *  *  *  ==================================\n  *  *  *  *  *    ================================== ");
+    }
+
+   for (int i = 0; i <= 6; i++) {
+     System.out.println(" ==================================================== ");
+    }
+
+ } // Method
+
+} // End Class
