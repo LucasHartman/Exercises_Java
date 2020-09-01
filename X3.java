7fb0ca732fd98044f811997dfbc31cfd26e18b41
laPronoia <70538655+laPronoia@users.noreply.github.com>
2020-09-01 10:43:06 +0200

    3. Divide two numbers
    3. Write a Java program to divide two numbers and print on the screen. Go to the editor
    Test Data :
    50/3
    Expected Output :
    16

---
+++ X3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class X3 {
+ public static void main(String[] args) {
+
+  DivFunction();
+
+  } // End Main
+
+//-----------------------------------------------------------------------------
+
+  static void DivFunction() {
+
+        //New Class Object
+       Scanner input = new Scanner (System.in);
+
+       // Keyword - input 1
+       System.out.print("Enter: ");
+       double input1 = input.nextDouble();
+
+       // Keyword - input2
+       System.out.print("Enter: ");
+       double input2 = input.nextDouble();
+
+       // Operator
+       double sum = input1 / input2;
+
+       // Print
+       System.out.println(input1 +" + " +input2 +" = " +sum);
+   } // End Method
+
+
+} // End Class
