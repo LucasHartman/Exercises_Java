592434cb58c6f58c075c1b4385ac8fe20fbf15b6
laPronoia <70538655+laPronoia@users.noreply.github.com>
2020-09-01 11:16:30 +0200

    23. convert a binary to hexadecimal
    23. Write a Java program to convert a binary number to hexadecimal number. Go to the editor
    Input Data:
    Input a Binary Number: 1101
    Expected Output
    
    HexaDecimal value: D

---
+++ X23.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList; // import the ArrayList class
+import java.util.*;
+import java.lang.Math;
+
+
+public class X23 {
+ public static void main(String[] args) {
+
+
+    Bin2Hex("1010");
+    // input Binaray value, output decimal value
+
+
+ } // End Main
+
+
+
+
+
+
+
+
+
+
+
+
+// Method ---------------------------------------------------------------------
+
+
+  static void Bin2Hex(String Bin) {
+
+// Input String Lenght
+int lenght = Bin.length();
+double sum = 0;
+
+
+for (int i = 0; i < lenght; i++) {
+
+    // Get Character from input
+    char ch1 = Bin.charAt(i);
+    // System.out.println("Character: " +ch1); // 1 0 1 0
+    // Character to Interger
+    int ch2 = Integer.parseInt(String.valueOf(ch1));
+    // System.out.println("Character: " +ch2); // 1 0 1 0
+
+
+    if (ch2 == 1) {
+
+          // Get index from Input
+          int index = lenght -1 -i;
+          //System.out.println("index: " +index);   // 3 2 1 0
+
+          double pow = Math.pow(2, index); // 2*2*2 & 2
+          // System.out.println(pow);
+
+          // add up values
+          sum += pow;
+
+      } // end if
+
+  } // end loop
+
+      // Result
+      System.out.println(sum);
+
+   } // End Method
+
+} // End Class
