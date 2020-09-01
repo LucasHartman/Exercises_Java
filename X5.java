aa40b8488fcccd510b6320e002275902888eca59
laPronoia <70538655+laPronoia@users.noreply.github.com>
2020-09-01 10:48:14 +0200

    5. The product of two numbers
    5. Write a Java program that takes two numbers as input and display the product of two numbers. Go to the editor
    Test Data:
    Input first number: 25
    Input second number: 5
    Expected Output :
    25 x 5 = 125

---
+++ X5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class X5 {
+ public static void main(String[] args) {
+
+  MulFunction();
+
+  } // End Main
+
+//-----------------------------------------------------------------------------
+
+  static void MulFunction() {
+
+        //New Class Object
+       Scanner input = new Scanner (System.in);
+
+       // Keyword - input 1
+       System.out.print("Enter: ");
+       double input1 = input.nextDouble();
+
+       // Keyword - input2
+       System.out.print("Enter: ");
+       double input2 = input.nextDouble();
+
+       // Operator
+       double sum = input1 * input2;
+
+       // Print
+       System.out.println(input1 +" x " +input2 +" = " +sum);
+   } // End Method
+
+
+} // End Class
