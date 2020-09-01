8611f79cb81cfcea2851b92fac6514a22106ab5a
laPronoia <70538655+laPronoia@users.noreply.github.com>
2020-09-01 10:58:55 +0200

    9. compute the specified expressions
    9. Write a Java program to compute the specified expressions and print the output. Go to the editor
    Test Data:
    ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
    Expected Output
    2.138888888888889

---
+++ X9.java
@@ -0,0 +1,9 @@
+public class X9 {
+ public static void main(String[] args) {
+
+  double sum = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+
+  System.out.println(sum);
+
+ } // End main
+} // End Class
