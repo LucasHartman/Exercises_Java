f9bced697e30646d63d51009476653cc13b92573
laPronoia <70538655+laPronoia@users.noreply.github.com>
2020-09-01 10:50:44 +0200

    6. sum (addition), multiply, subtract, divide and remainder of two numbers.
    6. Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers. Go to the editor
    Test Data:
    Input first number: 125
    Input second number: 24
    Expected Output :
    125 + 24 = 149
    125 - 24 = 101
    125 x 24 = 3000
    125 / 24 = 5
    125 mod 24 = 5

---
+++ X6.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class X6 {
+ public static void main(String[] args) {
+
+  OPsFunction();
+
+  } // End Main
+
+//-----------------------------------------------------------------------------
+
+  static void OPsFunction() {
+
+        //New Class Object
+       Scanner input = new Scanner (System.in);
+
+       // Keyword - input 1
+       System.out.print("Enter: ");
+       double input1 = input.nextDouble();
+
+       // Keyword - input2
+       System.out.print("Enter: ");
+       double input2 = input.nextDouble();
+
+       // Operator
+       double add = input1 + input2;
+       double sub = input1 - input2;
+       double mul = input1 * input2;
+       double div = input1 / input2;
+       double mod = input1 % input2;
+
+       // Print
+       System.out.println(input1 +" + " +input2 +" = " +add);
+       System.out.println(input1 +" - " +input2 +" = " +sub);
+       System.out.println(input1 +" * " +input2 +" = " +mul);
+       System.out.println(input1 +" / " +input2 +" = " +div);
+       System.out.println(input1 +" % " +input2 +" = " +mod);
+
+   } // End Method
+
+
+} // End Class
