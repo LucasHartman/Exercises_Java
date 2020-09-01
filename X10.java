c8f1b0998df3e152d2808dbb709f3cf687332fa3
laPronoia <70538655+laPronoia@users.noreply.github.com>
2020-09-01 11:01:10 +0200

    10. compute a specified formula
    10. Write a Java program to compute a specified formula. Go to the editor
    Specified Formula :
    4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
    Expected Output
    2.9760461760461765

---
+++ X10.java
@@ -0,0 +1,9 @@
+public class X10 {
+ public static void main(String[] args) {
+
+  double sum = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+
+  System.out.println(sum);
+
+ } // End main
+} // End Class
