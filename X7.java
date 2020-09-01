e1da90cfadbf2bb2e8d9de3cb3821280c7084b45
laPronoia <70538655+laPronoia@users.noreply.github.com>
2020-09-01 10:52:37 +0200

    7. number input multiplication table
    7. Write a Java program that takes a number as input and prints its multiplication table upto 10. Go to the editor
    Test Data:
    Input a number: 8
    Expected Output :
    8 x 1 = 8
    8 x 2 = 16
    8 x 3 = 24
    ...
    8 x 10 = 80

---
+++ X7.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class X7 {
+ public static void main(String[] args) {
+
+  TableFunction();
+
+  } // End Main
+
+//-----------------------------------------------------------------------------
+
+  static void TableFunction() {
+
+        //New Class Object
+       Scanner input = new Scanner (System.in);
+
+       // Keyword - input 1
+       System.out.print("Enter: ");
+       int input1 = input.nextInt();
+
+       int mul = 0;
+
+       for(int i=1; i<=10; i++) {
+         mul = input1 * i;
+         System.out.println(input1 +" x " +i +" = " +mul);
+       } // End Loop
+
+   } // End Method
+
+
+} // End Class
